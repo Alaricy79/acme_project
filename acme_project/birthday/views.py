d642fc1a0587726b40bbc28a55d6dd0573025503
Roman V. Yudanov <Alaricy@gmail.com>
2025-12-04 20:27:28 +0200

    autorisations done

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -3,6 +3,8 @@ from django.views.generic import (CreateView, DeleteView, ListView,
                                   UpdateView, DetailView)
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
 
 # Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
 from .forms import BirthdayForm
@@ -10,17 +12,30 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-class BirthdayCreateView(CreateView):
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
+
+
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
+    form_class = BirthdayForm
+    model = Birthday
+
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)
+
+
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     form_class = BirthdayForm
     model = Birthday
 
 
-class BirthdayUpdateView(UpdateView):
-    form_class = BirthdayForm
-    model = Birthday
-
-
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
